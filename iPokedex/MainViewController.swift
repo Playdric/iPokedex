21d17dd0b09968471edd2d6b52e350c189699410
Administrator <administrator@macintosh.local>
2018-01-02 17:41:19 +0100

    Commit mainViewController : Constraint OK + Ajout ImageView (colour palette à decider)

--- iPokedex/MainViewController.swift
+++ iPokedex/MainViewController.swift
@@ -10,26 +10,68 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchImageView: UIImageView!
+    @IBOutlet weak var pokedexImageView: UIImageView!
+    @IBOutlet weak var battleImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        
+        //Url de l'API
+        let endpoint : String = "http://pokeapi.co/api/v2/pokemon/"
+        
+        //test de la validité de l'url
+        guard let url = URL(string: endpoint) else{
+            print("error : cannot create URL")
+            return
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        //Pour envoyer une request il nous faut une session (doc à lire sur Shared mais c'est par défaut)
+        let session = URLSession.shared
+        
+        //Création de la task avec comme param l'url et la gestion du retour (completionHandler)
+        let task = session.dataTask(with: urlRequest){ data, response, error in
+            
+            
+            //test sur l'existence d'une erreur
+            guard error == nil else{
+                print("error calling the url")
+                return
+            }
+            
+            
+            //test sur l'existence des données
+            guard let responseData = data else {
+                print("error did not receive data from url")
+                return
+            }
+            
+            do{
+                //Parsing de la réponse en JSON parce que c'est ce que l'API nous renvoie
+                guard let pokemon = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: Any] else{
+                    print("error trying to convert data to JSON")
+                    return
+                }
+                
+                //Si on arrive ici alors on a le resultat de l'API
+                print("Le pokemon est: "+pokemon.description)
+                
+                
+                
+            }catch{
+                print("error trying to convert data to JSON")
+                return
+            }
+            
+        }
+        
+        task.resume()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
